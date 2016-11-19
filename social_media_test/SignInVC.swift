7d94fb30534108311ed85515a4adb88c71bff208
hnine <hnine@hnine-ui-MacBook-Air.local>
2016-11-20 04:24:45 +0900

    initial commit

---
+++ social_media_test/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social_media_test
+//
+//  Created by hnine on 2016. 11. 17..
+//  Copyright © 2016년 hnine. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
