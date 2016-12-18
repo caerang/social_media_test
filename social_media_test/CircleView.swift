42a2d635060aed4da2092e6cbc31594d9d33137d
LEEHYEONG DO <hyeongdo@LEEHYEONGui-MacBook-Pro.local>
2016-12-18 14:05:05 +0900

    completed feed-ui

---
+++ social_media_test/CircleView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleView.swift
+//  social_media_test
+//
+//  Created by LEEHYEONG DO on 2016. 12. 18..
+//  Copyright © 2016년 hnine. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+
+}
